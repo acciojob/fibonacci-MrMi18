b7dab3d27bd64ab4199756f9e36239e9146d195e
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2024-02-28 10:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	let fib = 1;
+	if(num==0){
+		console.log(0);
+	}
+	if(num==1){
+		console.log(1);
+	}
+	
+	for(let i = 2; i<num ;i++){
+		fib+=i;
+    }
+	console.log(fib);
 }
 
 module.exports = fibonacci;
